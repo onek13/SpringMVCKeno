010651f25e038576aba00bd68dd9a8d9cec03a36
Lenovo G480 <Lenovo G480@LenovoG480-PC>
2016-12-21 22:04:12 +0800

    2nd commit

--- SpringMVC_Keno/src/com/training/customer/controller/CustomerController.java
+++ SpringMVC_Keno/src/com/training/customer/controller/CustomerController.java
@@ -3,6 +3,8 @@ package com.training.customer.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//Test 2nd Commit Github
+
 @Controller
 public class CustomerController {
 
